ad175bd5be0f1e087e50b8959dda04f46d9fda40
Cameron Thompson <camo.thommo@gmail.com>
2018-10-05 13:22:58 +1000

    my entire page

--- PwF/PwF/PwF/CharacterCreation/RaceCode.cs
+++ PwF/PwF/PwF/CharacterCreation/RaceCode.cs
@@ -1,6 +1,9 @@
-﻿using PwF.Cells;
+﻿using Newtonsoft.Json;
+using PwF.Cells;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace PwF.CharacterCreation
@@ -8,14 +11,22 @@ namespace PwF.CharacterCreation
     public static class RaceCode {
         public static string[] getRaces() {
 
-            /*var assembly = typeof(RacePage).GetTypeInfo().Assembly;
-            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Information.json");
+            //var assembly = typeof(RacePage).GetTypeInfo().Assembly;
+            ////Stream stream = assembly.GetManifestResourceStream("Information.json");
+            //Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Your_File.json");
 
-            using (var reader = new System.IO.StreamReader(stream)) {
+            //using (var reader = new System.IO.StreamReader(stream)) {
 
-                var json = reader.ReadToEnd();
-                var data = JsonConvert.DeserializeObject(json);
-            }*/
+            //    var json = reader.ReadToEnd();
+            //    var data = JsonConvert.DeserializeObject(json);
+            //}
+
+            //var assembly = IntrospectionExtensions.GetTypeInfo(typeof(LoadResourceText)).Assembly;
+            //Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.PCLTextResource.txt");
+            //string text = "";
+            //using (var reader = new System.IO.StreamReader(stream)) {
+            //    text = reader.ReadToEnd();
+            //}
 
             string[] value = new string[] {
                 "Dwarves", "Elves", "Gnomes", "Half-Elves", "Half-Orcs",
