a5b85fab64d5e6866bbd0ab706e059993e9c95d2
Zackary Griffin <zackarygriffin20@gmail.com>
2018-10-11 09:03:01 +1000

    Details save to view model

---
+++ PwF/PwF/PwF/CharacterCreation/DetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PwF.CharacterCreation
+{
+    class DetailsViewModel
+    {
+        public String SelectedName { get; set; }
+        public String SelectedAlignment { get; set; }
+        public String SelectedDeity { get; set; }
+        public String SelectedHomeland { get; set; }
+        public String SelectedSize { get; set; }
+        public String SelectedGender { get; set; }
+        public String SelectedAge { get; set; }
+        public String SelectedHeight { get; set; }
+        public String SelectedWeight { get; set; }
+        public String SelectedHair { get; set; }
+        public String SelectedEyes { get; set; }
+
+    }
+}
