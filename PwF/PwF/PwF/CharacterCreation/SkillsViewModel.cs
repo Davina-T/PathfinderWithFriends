5a44ee6ea6bbfe95e8e2155709287378c2c69393
Zackary Griffin <zackarygriffin20@gmail.com>
2018-10-19 13:05:31 +1000

    Update PwF/PwF/PwF/CharacterCreation/SkillsViewModel.cs

--- PwF/PwF/PwF/CharacterCreation/SkillsViewModel.cs
+++ PwF/PwF/PwF/CharacterCreation/SkillsViewModel.cs
@@ -10,20 +10,17 @@ namespace PwF.CharacterCreation
     class SkillsViewModel
     {
         private PageNavigationManager navManager;
-        public Skills SkillValues { get; set; }
         private int remainingPoints { get; set; }
 
         public SkillsViewModel()
         {
             navManager = PageNavigationManager.Instance;
-            SkillValues = new Skills();
         }
 
         public void NextPage()
         {
             if (remainingPoints == 0)
             {
-                Statics.CharacterCreating.CreatingCharacter.Skills = SkillValues;
                 navManager.ShowFeatsPage();
             }
         }
@@ -37,20 +34,19 @@ namespace PwF.CharacterCreation
         {
             // open the informative page
         }
-
+        /*
         public void IncreaseAcrobatics()
         {
             if (remainingPoints != 0)
             {
-                SkillValues.Acrobatics.Value += 1;
-                remainingPoints -= 1;
+
             }
         }
 
         public void DecreaseAcrobatics()
         {
-            SkillValues.Acrobatics.Value -= 1;
-            remainingPoints += 1;
+
         }
+        */
     }
 }
