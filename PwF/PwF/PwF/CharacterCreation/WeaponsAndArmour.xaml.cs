3efbfc1de15c9ae647e183655874f0611f93dc9c
Christopher Littlewood <christopher.littlewood@connect.qut.edu.au>
2018-10-23 12:11:08 +1000

    Skeleton for Weapons and Armour Page

---
+++ PwF/PwF/PwF/CharacterCreation/WeaponsAndArmour.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PwF.CharacterCreation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeaponsAndArmour : ContentPage
+    {
+
+        WeaponsAndArmourViewModel viewModel = new WeaponsAndArmourViewModel();
+
+        public WeaponsAndArmour()
+        {
+            InitializeComponent();
+
+
+            // add the binding for the right arrow and a tap recognizer
+            RightArrow.BindingContext = viewModel;
+            var tapGestureRecognizer1 = new TapGestureRecognizer();
+            tapGestureRecognizer1.Tapped += (s, e) => {
+                //DisplayAlert("Alert", "Next Page", "OK");
+                viewModel.NextPage();
+            };
+            RightArrow.GestureRecognizers.Add(tapGestureRecognizer1);
+
+            // add the binding for the left arrow and a tap recognizer
+            LeftArrow.BindingContext = viewModel;
+            var tapGestureRecognizer2 = new TapGestureRecognizer();
+            tapGestureRecognizer2.Tapped += (s, e) => {
+                //DisplayAlert("Alert", "Previous Page", "OK");
+                viewModel.PrevPage();
+            };
+            LeftArrow.GestureRecognizers.Add(tapGestureRecognizer2);
+
+            // add the binding for the InfoButton and a tap recognizer
+            InfoButton.BindingContext = viewModel;
+            var tapGestureRecognizer3 = new TapGestureRecognizer();
+            tapGestureRecognizer3.Tapped += (s, e) => {
+                //DisplayAlert("Alert", "Info Page", "OK");
+                viewModel.ViewInfo();
+            };
+            InfoButton.GestureRecognizers.Add(tapGestureRecognizer3);
+        }
+    }
+}
---
+++ PwF/PwF/PwF/CharacterCreation/WeaponsAndArmourViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Pwf.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PwF.CharacterCreation
+{
+    public class WeaponsAndArmourViewModel
+    {
+        private PageNavigationManager navManager;
+
+        public WeaponsAndArmourViewModel()
+        {
+            navManager = PageNavigationManager.Instance;
+
+        }
+
+        public void NextPage()
+        {
+            //if (SelectedRace != null && SelectedRace.Title != "") {
+            //    Statics.CharacterCreating.CreatingCharacter.Race = SelectedRace.Title;
+            //    navManager.ShowClassPage();
+            //}
+        }
+
+        public void PrevPage()
+        {
+            //Statics.CharacterCreating.CreatingCharacter.Race = null;
+            //navManager.ShowLevelPage(false);
+        }
+
+        public void ViewInfo()
+        {
+            // open the informative page
+        }
+    }
+}
+
